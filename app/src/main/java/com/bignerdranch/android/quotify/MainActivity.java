634ae5a6d06302d7893bcc4810fe533340628dc2
sirp140 <sirpreetkaur1@gmail.com>
2023-05-31 19:55:26 -0700

    -added details button which start second activity to show quote description

--- app/src/main/java/com/bignerdranch/android/quotify/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/quotify/MainActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.quotify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,13 +20,14 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton mPrevButton;
     private TextView mQuoteTextView;
     private int mCurrentIndex = 0;
+    private Button mDetailsButton;
 
     private Quote[] mQuoteBank = new Quote[]{
-            new Quote(R.string.believe_you_can),
-            new Quote(R.string.harder_I_work),
-            new Quote(R.string.predict_the_future),
-            new Quote(R.string.keep_going),
-            new Quote(R.string.future_belongs_to),
+            new Quote(R.string.believe_you_can, R.string.quote_believe_description),
+            new Quote(R.string.harder_I_work, R.string.quote_harder_work_description),
+            new Quote(R.string.predict_the_future, R.string.quote_predict_future_description),
+            new Quote(R.string.keep_going, R.string.quote_keep_going_description),
+            new Quote(R.string.future_belongs_to, R.string.quote_future_belongs_description),
     };
 
     //onCreate(Bundle) method is called when an instance of the activity subclass is created
@@ -40,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         mNextButton = findViewById(R.id.next_button);
         mQuoteTextView = (TextView) findViewById(R.id.quote_text_view);
         mPrevButton = findViewById(R.id.prev_button);
+        mDetailsButton = (Button)findViewById(R.id.details_button);
 
         //setting listeners for yes button
         mYesButton.setOnClickListener(new View.OnClickListener() {
@@ -79,6 +82,23 @@ public class MainActivity extends AppCompatActivity {
                     mCurrentIndex = (mCurrentIndex -1) % mQuoteBank.length;
                 }
                 updateQuote();
+            }
+        });
+
+        mDetailsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                //get the current quote
+                Quote currentQuote = mQuoteBank[mCurrentIndex];
+
+                //get the quote details resource id
+                int quoteDetailsResId = currentQuote.getQuoteDetails();
+
+                //start DetailsActivity with the quote details resource ID
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                intent.putExtra("QUOTE_DETAILS_RES_ID", quoteDetailsResId);
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/bignerdranch/android/quotify/Quote.java
+++ app/src/main/java/com/bignerdranch/android/quotify/Quote.java
@@ -2,20 +2,37 @@ package com.bignerdranch.android.quotify;
 
 public class Quote {
     private int mTextResId;
-    //private boolean mAnswerTrue;
+    private int mquoteDetailsResId;
 
-    public Quote (int textResId){
+
+    public Quote (int textResId, int quoteDetailsResId){
         mTextResId = textResId;
-        //mAnswerTrue = answerTrue;
+        mquoteDetailsResId = quoteDetailsResId;
     }
 
     public int getTextResId() {
         return mTextResId;
     }
 
+
+    public int getQuoteDetails(){
+        return mquoteDetailsResId;
+    }
+
+
+
     public void setTextResId(int textResId){
         mTextResId = textResId;
     }
+
+
+    public void setQuoteDetails(int quoteDetailsResId){
+        mquoteDetailsResId = quoteDetailsResId;
+    }
+
+
+
+
 
     /* public boolean isAnswerTrue() {
         return mAnswerTrue;
